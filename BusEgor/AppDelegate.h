a69e74d5b671736de726126f786b6052b07ac617
aadudyrev <aadudyrev@gmail.com>
2016-12-05 15:15:13 +0500

    Initial Commit

---
+++ BusEgor/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BusEgor
+//
+//  Created by Александр Дудырев on 05.12.16.
+//  Copyright © 2016 Александр Дудырев. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
